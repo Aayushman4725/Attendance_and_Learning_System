12976c21df6a90ca10c4707e0553224d2ddc3a6a
Aayushman4725 <aayushmannp@gmail.com>
2025-05-21 22:02:11 +0545

    added views and urls for adding attendance and showing user group

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import attendance
+from .views import attendance, attendance_user_group, attendance_create
 
 urlpatterns = [
     path('attendance_date/', attendance, name='attendance'),
+    path('attendance_user_group/<int:pk>/', attendance_user_group, name='attendance_user_group'),
+    path('attendance_create/', attendance_create, name='attendance_create'),
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -14,3 +14,27 @@ def attendance(request):
         attendance_data = Attendance.objects.all()
         serializer = AttendanceSerializer(attendance_data, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def attendance_user_group(request,pk):
+    if request.method == 'GET':
+        attendance_data = Attendance.objects.filter(user_group_id = pk)
+        serializer = AttendanceSerializer(attendance_data, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def attendance_create(request):
+    if request.method == 'POST':
+        serializers = AttendanceSerializer(data=request.data)
+        
+        if serializers.is_valid():
+            serializers.save()
+            return Response(
+                {
+                    "data": serializers.data,
+                    "message": "Student attendance taken"
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
