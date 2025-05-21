301289d26f482001ab2ec5ffa1c848104d71b9d4
Aayushman4725 <aayushmannp@gmail.com>
2025-05-21 22:01:25 +0545

    added some changes in attendance serializer

--- attendance/serializers.py
+++ attendance/serializers.py
@@ -29,8 +29,12 @@ class ScheduleSerializer(serializers.ModelSerializer):
         
 class AttendanceSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all())
-    date = serializers.DateField()
-    check_in = serializers.TimeField(required=False)
+    user_name = serializers.CharField(source = 'user.username', read_only=True)
+    user_group = serializers.PrimaryKeyRelatedField(queryset=UserGroup.objects.all())
+    group_name = serializers.CharField(source = 'user_group.group.name', read_only=True)
+    course_name = serializers.CharField(source = 'user_group.course.name', read_only=True)
+    date = serializers.DateField(read_only=True)
+    check_in = serializers.DateTimeField(read_only=True)
     status = serializers.ChoiceField(choices=[('present', 'Present'), ('absent', 'Absent'), ('leave', 'Leave'), ('late', 'Late')])
     note = serializers.CharField(required=False, allow_blank=True)
     
@@ -38,3 +42,11 @@ class AttendanceSerializer(serializers.ModelSerializer):
         model = Attendance
         fields = '__all__'
         read_only_fields = ['id']
+    
+    def validate(self, attrs):
+        user = attrs.get('user')
+        user_group = attrs.get('user_group')
+        
+        if user and user_group and not user_group.user.filter(id = user.id).exists():
+            raise serializers.ValidationError("User does not belong to this group.")
+        return attrs
