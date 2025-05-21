5d4a286a7d79b450273e7204e31486f23f5aa07a
Aayushman4725 <aayushmannp@gmail.com>
2025-05-21 22:00:40 +0545

    added user list in user group

--- attendance/admin.py
+++ attendance/admin.py
@@ -3,7 +3,11 @@ from .models import Department, Group, UserGroup, Schedule, Attendance,Course
 # Register your models here.
 admin.site.register(Department)
 admin.site.register(Group)  
-admin.site.register(UserGroup)
+# admin.site.register(UserGroup)
 admin.site.register(Schedule)
 admin.site.register(Attendance)
 admin.site.register(Course)
+
+@admin.register(UserGroup)
+class UserGroupAdmin(admin.ModelAdmin):
+    filter_horizontal = ('user',)  # So you can pick users in a widget
